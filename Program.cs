8b9da7d2e77c10d1e9019fc3cc01ccc5756c206e
Alexander <mypost@example.com>
2023-01-15 16:38:29 +0300

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// task 38
+void FillArrayRandomNumbers(double[] nums)
+{
+    for(int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+        }
+}
+
+Console.Clear();
+Console.WriteLine("Укажите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] nums = new double[size];
+FillArrayRandomNumbers(nums);
+Console.WriteLine($" [ {String.Join(" ", nums)}]");
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] > max)
+        {
+            max = nums[i];
+        }
+    if (nums[i] < min)
+        {
+            min = nums[i];
+        }
+}
+
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"Разность max-min значений = {max - min}");
